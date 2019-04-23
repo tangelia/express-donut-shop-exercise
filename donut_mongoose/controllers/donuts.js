a514cfea0345af82452521322d92cb041e924701
tangelia <tangelia.atlanta@gmail.com>
2019-04-22 22:36:43 -0400

    first commit

---
+++ donut_mongoose/controllers/donuts.js
@@ -0,0 +1,51 @@
+//= =====================
+// REQUIREMENTS
+//= =====================
+// require the Donut model
+
+const donutController = {
+  //= =====================
+  // INDEX
+  //= =====================
+  // Create a function sends all Donuts to donuts/index.hbs view
+
+  //= =====================
+  // NEW
+  //= =====================
+  // Create a function that renders the new.hbs form
+
+  //= =====================
+  // SHOW
+  //= =====================
+  // Create a function that renders a single Donut's show page
+
+  //= =====================
+  // CREATE
+  //= =====================
+  // Create a function that creates a new Donut
+  // and upon success redirects back to the index page "/"
+
+  //= =====================
+  // EDIT
+  //= =====================
+  // Create a function that renders the edit.hbs page and
+  // sends that a Donut's data to it
+
+  //= =====================
+  // UPDATE
+  //= =====================
+  // Create a function that updates the Donut and
+  // redirects back to the SHOW PAGE (not index)
+
+  //= =====================
+  // DELETE
+  //= =====================
+  // Create a function that deletes the Donut and
+  // redirects back to index page "/"
+
+}
+
+//= =====================
+// EXPORTS
+//= =====================
+// export the controller with module.exports
---
+++ donut_mongoose/routes/index.js
@@ -0,0 +1,57 @@
+let express = require('express')
+let router = express.Router()
+
+// Example Route
+// router.get('/', someController.index)
+
+//= =====================
+// INDEX
+//= =====================
+// Create a GET index route "/" that triggers the donut controller index function
+
+
+
+//= =====================
+// NEW
+//= =====================
+// Create a GET new route "/new" that triggers the donut controller new function
+
+
+
+//= =====================
+// SHOW
+//= =====================
+// Create a GET show route "/:id" that triggers the donut controller show function
+
+
+
+
+//= =====================
+// CREATE
+//= =====================
+// Create a POST index route "/" that triggers the donut controller create function
+
+
+
+//= =====================
+// EDIT
+//= =====================
+// Create a GET edit route "/:id/edit" that triggers the donut controller edit function
+
+
+
+//= =====================
+// UPDATE
+//= =====================
+// Create a PUT update route "/:id" that triggers the donut controller update function
+
+// Create a PATCH update route "/:id" that triggers the donut controller update function
+
+
+//= =====================
+// DELETE
+//= =====================
+// Create a DELETE delete route "/:id" that triggers the donut controller delete function
+
+
+module.exports = router
