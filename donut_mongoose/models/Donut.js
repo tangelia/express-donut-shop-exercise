ca3237db8034063da552751bc5526b6af1aff2da
tangelia <tangelia.atlanta@gmail.com>
2019-04-23 00:25:16 -0400

    created donut schema

--- donut_mongoose/models/Donut.js
+++ donut_mongoose/models/Donut.js
@@ -1,7 +1,17 @@
 // requirements: require db/connection as 'mongoose'
-
+const mongoose = require('./connection.js');
 
 // create your donut schema:
-
+const Donut = new Schema({
+    //write your schema fields here
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    img: String,
+    price: Number,
+    qty: Number
+    });
 
 // export the donut model with module.exports
+module.exports = {
+
+}
